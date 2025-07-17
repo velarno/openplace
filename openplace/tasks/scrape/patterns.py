da44e1cda9ae0d50425b82d56f1fd1af1c50204e
Arno VELETANLIC <git@arnov.dev>
2025-07-17 11:55:39 +0200

    wip: updated BOAMP regex to match both http & https URLs

--- openplace/tasks/scrape/patterns.py
+++ openplace/tasks/scrape/patterns.py
@@ -3,7 +3,7 @@ Patterns used for scraping the website urls and maintaining the state of the scr
 """
 
 REGLEMENT_REGEX = r'^/index.php\?page=Entreprise\.EntrepriseDownloadReglement&id=([a-zA-Z\d=]+)&orgAcronyme=([\da-z]+)$'
-BOAMP_REGEX = r'^https://www\.boamp\.fr/(?:index\.php/)?avis/detail/([\d-]+)(?:/[\d]+)?$'
+BOAMP_REGEX = r'^https?://www\.boamp\.fr/(?:index\.php/)?avis/detail/([\d-]+)(?:/[\d]+)?$'
 URL_SEARCH = 'https://www.marches-publics.gouv.fr/?page=Entreprise.EntrepriseAdvancedSearch&AllCons'
 LINK_REGEX = r'^https://www\.marches-publics\.gouv\.fr/app\.php/entreprise/consultation/([\d]+)\?orgAcronyme=([\da-z]+)$'
 PAGE_STATE_REGEX = 'name="PRADO_PAGESTATE" id="PRADO_PAGESTATE" value="([a-zA-Z0-9/+=]+)"'
